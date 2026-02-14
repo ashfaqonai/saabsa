418c9bea429b9fbf523709954bcf0d8c2e640bb5
Ashfaq Ahmed <ashfaq.onai@gmail.com>
2026-02-13 18:10:42 -0600

    Integrate Tailwind CSS build process and optimize HTML files
    - Added a step to build Tailwind CSS and replace CDN links with optimized CSS in all HTML files.
    - Ensured that the generated CSS is minified for improved performance.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './blog/*.html',
+    './scripts/build-blog.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
